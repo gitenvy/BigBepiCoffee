2df3f9fb736ebc0439db37e3b868230a75106c4c
Bepi <geraldogs.93@gmail.com>
2025-11-10 22:45:00 +0700

    Updated CoffeeBean and EspressoBasedDrink enums

--- Project.java
+++ Project.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-enum coffeeType {
+enum EspressoBasedDrink {
     LONG_BLACK,
     CAPPUCINO,
     FLAT_WHITE,
@@ -9,26 +12,32 @@ enum coffeeType {
     // TODO : add prices as enum attributes
 }
 
-enum roastLevel {
+enum RoastLevel {
     LIGHT,
     MEDIUM,
     MEDIUM_DARK,
     DARK
 }
 
-enum shotType {
+enum ShotType {
     ESPRESSO,
     RISTRETTO,
     LUNGO
 }
 
-enum processingMethod {
+enum ProcessingMethod {
     WASHED,
     NATURAL,
     ANAEROBIC_NATURAL,
     ANAEROBIC_WASHED,
     EXPERIMENTAL_WASHED,
     LUWAK
+}
+
+enum Species {
+    ARABICA,
+    ROBUSTA,
+    LIBERICA
 }
 
 
@@ -49,26 +58,97 @@ class Barista {
 
 abstract class CoffeeBean {
 
-    String roastLevel;
+    RoastLevel roastLevel;
     String origin; 
-    String processingMethod;
+    ProcessingMethod processingMethod;
+    int altitude; // For example, 2000 MASL
+    Species species;
+    String supplier;
 
+
+
+
+    
     public String toString () {
         return this.roastLevel + " | Origin: " + this.origin + " | " + this.processingMethod; 
     }
 }
 
 
-abstract class menuOrder {
 
-    int coffeeType; // Use enum
+class espressoBlend {
+
+    List<blendComponent> blend;
+
+
+    espressoBlend() {
+        List<blendComponent> blend = new ArrayList<>();
+    }
+
+    
+
+    
+}
+
+
+class blendComponent {
+    
+
+    CoffeeBean coffeeBean;
+    double percentage;
+
+    blendComponent(CoffeeBean newCoffeeBean, double percentage) {
+        this.coffeeBean = newCoffeeBean;
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String toString() {
+        return "blendComponent [coffeeBean=" + coffeeBean + ", percentage=" + percentage + "]";
+    }
+
+
+    
+}
+
+
+class singleOriginBean {
+    
+
+   // Thinking how it will be different in the main code..
+
+
+}
+
+
+
+
+
+abstract class menuItem {
+
     int quantity;
+    int price;
 
 } 
 
 
-class espressoBasedOrder extends menuOrder {
 
-    String shotType 
+
+class espressoBasedOrder extends menuItem {
+
+    ShotType shotType;
+    EspressoBasedDrink espressoBasedOrder;
+
+
+    // Store things like strength, bitterness in the enum!
+
 
 }
+
+class filterBasedOrder extends menuItem {
+
+    int dripper;
+    int paperFilter;
+    
+}
+
